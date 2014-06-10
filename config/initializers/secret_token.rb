5ad6a5e269a78d385802747fc19d3ae5c7747452
Morgan Krey <morgankrey@gmail.com>
2014-06-10 09:54:42 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dopebeats::Application.config.secret_key_base = 'bba5723c1464a308440e32e4b7ec76fba637f436c65b479b117288b347e848de250b687c7ca273226947355a890a5f62345b6ac456b5e9beef09d8f0cd4d8bfc'
